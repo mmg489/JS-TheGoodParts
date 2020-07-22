dbd5c08c92d6210aedcc9b80c170ac893fe43705
Margarita <m_mg489@outlook.com>
2020-07-21 23:56:05 -0400

    added blank style sheet and basic head html data for weather APP exercise

--- program.js
+++ program.js
@@ -1,7 +1,9 @@
-document.writeln("Hello, World!");
+//document.writeln("Hello, World!");
 
 // the isNan(number) function tells you if a value is a number or not,
 // if you type a number in the pass through it will return false because the value is an actual number,
 // however if you were to type a value that is not a number i.e. a string, words, dates, etc.
 //you would get a value of true because those values are not numbers.
-console.log(isNaN("hello"));
+//console.log(isNaN("hello"));
+
+//practice with weather APP
