ba74f8a46e2a5560358bd02e2ca68d004f93b342
Margarita <m_mg489@outlook.com>
2020-07-22 00:54:04 -0400

    updated code in js file to pull weather data

--- program.js
+++ program.js
@@ -7,3 +7,30 @@
 //console.log(isNaN("hello"));
 
 //practice with weather APP
+
+//we need to get the longitude and latitude of our user location
+//the way to do it is built into JavaScript
+
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  //this will only work if the user allows it when the popup comes up
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const api = `http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={697e7e756f4ba3e54ff32639dc254b53}`;
+
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+        });
+    });
+  } else {
+    h1.textContent = "Location Unavailable please allow geolocation";
+  }
+});
